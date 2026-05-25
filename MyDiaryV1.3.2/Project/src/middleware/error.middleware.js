b8922d21f3b66a1bf418d4bae92c5cd679239e8b
MaxDerk <derkach.maksym@chnu.edu.ua>
2026-05-25 11:59:53 +0300

    Delete MyDiaryV1.3.2 directory

--- MyDiaryV1.3.2/Project/src/middleware/error.middleware.js
+++
@@ -1,7 +0,0 @@
-function errorHandler(error, req, res, next) {
-    const status = error.statusCode || 500;
-    console.error(`[Error]: ${error.message}`);
-    const message = error.message || 'Server Error';
-    res.status(status).json({ error: message });
-}
-module.exports = errorHandler;
